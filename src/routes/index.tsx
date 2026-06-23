39b8ed677f69fea46da8e1f63ca62d46e33cd003
Kankati Mokshagna Anurag <116142650+MokshagnaAnurag@users.noreply.github.com>
2026-06-23 10:13:42 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ import { Education } from "@/components/portfolio/Education";
 import { Publications } from "@/components/portfolio/Publications";
 import { Blogs } from "@/components/portfolio/Blogs";
 import { About } from "@/components/portfolio/About";
+import { NewsTicker } from "@/components/portfolio/NewsTicker";
 import { Skills } from "@/components/portfolio/Skills";
 import { Contact } from "@/components/portfolio/Contact";
 import { Footer } from "@/components/portfolio/Footer";
@@ -38,15 +39,17 @@ function Index() {
       <ScrollProgress />
       <Navbar />
       <main>
-        <ToonhubHero />
-        <About />
-        <SelectedWork />      // Projects
-        <Publications />
-        <Experience />
-        <Education />
-        <Skills />
-        <Blogs />
-        <Contact />
+        <div className="cursor-orange"><ToonhubHero /></div>
+        <div className="cursor-blue">
+          <About />
+        </div>
+        <div className="cursor-magenta"><SelectedWork /></div>
+        <div className="cursor-yellow"><Publications /></div>
+        <div className="cursor-cyan"><Experience /></div>
+        <div className="cursor-green"><Education /></div>
+        <div className="cursor-orange"><Skills /></div>
+        <div className="cursor-blue"><Blogs /></div>
+        <div className="cursor-magenta"><Contact /></div>
       </main>
       <Footer />
     </div>
